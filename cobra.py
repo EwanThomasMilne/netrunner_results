4662e15110ece545bac4466d3397efc79f6f9669
not_yeti <ewanmilneu@gmail.com>
2024-05-06 21:07:16 +0100

    now produces one report which sums up all the tournament results

--- cobra.py
+++ cobra.py
@@ -11,81 +11,84 @@ class Players:
             if player['id'] == player_id:
                 return player[role+'Identity']
 
+
+class Identities:
+    
+    def __init__(self) -> None:
+            
+        self.identities = {}
+            
+    def add_result(self, identity: str, result: str):
+        
+        if identity not in self.identities:
+            self.identities.update({identity: {'wins': 0, 'draws': 0, 'loses': 0}})
+            
+        self.identities[identity][result] += 1
+
+
+class Results:
+    
+    def __init__(self) -> None:
+        self.identities = Identities()
+        
+    def tally_swiss_match(self, match, players):
+        
+        player1runner = players.get_identity(player_id=match['player1']['id'], role='runner')
+        player1corp =  players.get_identity(player_id=match['player1']['id'], role='corp')
+        player2runner =  players.get_identity(player_id=match['player2']['id'], role='runner')
+        player2corp =  players.get_identity(player_id=match['player2']['id'], role='corp')
+        
+        #player 1 wins thier runner game
+        if match['player1']['runnerScore'] == 3:
+            self.identities.add_result(player1runner, 'wins')
+            self.identities.add_result(player2corp, 'loses')
+        # draw
+        elif match['player1']['runnerScore'] == 1:
+            self.identities.add_result(player1runner, 'draws')
+            self.identities.add_result(player2corp, 'draws')
+        #player 1 loses thier runner game
+        elif match['player1']['runnerScore'] == 0:
+            self.identities.add_result(player1runner, 'loses')
+            self.identities.add_result(player2corp, 'wins')
+            
+        #player 1 wins thier corp game
+        if match['player1']['corpScore'] == 3:
+            self.identities.add_result(player1corp, 'wins')
+            self.identities.add_result(player2runner, 'loses')
+        # draw
+        elif match['player1']['corpScore'] == 1:
+            self.identities.add_result(player1corp, 'draws')
+            self.identities.add_result(player2runner, 'draws')
+        #player 1 loses thier corp game
+        elif match['player1']['corpScore'] == 0:
+            self.identities.add_result(player1corp, 'loses')
+            self.identities.add_result(player2runner, 'wins')
+
+    def tally_cut_match(self, match, players):
+        
+        if match['player1']['winner']:
+            winner = 'player1'
+            loser = 'player2'
+        else:
+            winner = 'player2'
+            loser = 'player2'
+            
+        self.identities.add_result(players.get_identity(player_id=match[winner]['id'], role=match[winner]['role']), 'wins')
+        self.identities.add_result(players.get_identity(player_id=match[loser]['id'], role=match[loser]['role']), 'loses')
+
+    def tally_results(self, rounds, players_dict):
+        
+        players = Players(players_dict)
+        
+        for round in rounds:
+            for match in round:
+                if match['eliminationGame']: 
+                    self.tally_cut_match(match=match, players=players)
+                else:
+                    self.tally_swiss_match(match=match, players=players)
+
+
 def get_cobra_json(url: str):
     # get the results from cobra from a URL of a given tournament
     resp = requests.get(url=url+'.json', params='')
     return resp.json()
-
-def create_id_list(players):
-    #create a dictionary of all IDs played
-    id_list = {}
-    for player in players:
-        id_list.update({player['runnerIdentity']: {'wins': 0, 'draws': 0, 'loses': 0}})
-        id_list.update({player['corpIdentity']: {'wins': 0, 'draws': 0, 'loses': 0}})
-        
-    return id_list
-
-def tally_swiss_match(match, players, id_list):
-        
-    player1runner = players.get_identity(player_id=match['player1']['id'], role='runner')
-    player1corp =  players.get_identity(player_id=match['player1']['id'], role='corp')
-    player2runner =  players.get_identity(player_id=match['player2']['id'], role='runner')
-    player2corp =  players.get_identity(player_id=match['player2']['id'], role='corp')
-    
-    #player 1 wins thier runner game
-    if match['player1']['runnerScore'] == 3:
-        id_list[player1runner]['wins'] += 1
-        id_list[player2corp]['loses'] += 1
-    # draw
-    elif match['player1']['runnerScore'] == 1:
-        id_list[player1runner]['draws'] += 1
-        id_list[player2corp]['draws'] += 1
-    #player 1 loses thier runner game
-    elif match['player1']['runnerScore'] == 0:
-        id_list[player1runner]['loses'] += 1
-        id_list[player2corp]['wins'] += 1
-    else:
-        print("error")
-        
-    #player 1 wins thier corp game
-    if match['player1']['corpScore'] == 3:
-        id_list[player1corp]['wins'] += 1
-        id_list[player2runner]['loses'] += 1
-    # draw
-    elif match['player1']['corpScore'] == 1:
-        id_list[player1corp]['draws'] += 1
-        id_list[player2runner]['draws'] += 1
-    #player 1 loses thier corp game
-    elif match['player1']['corpScore'] == 0:
-        id_list[player1corp]['loses'] += 1
-        id_list[player2runner]['wins'] += 1
-    else:
-        print("error")
-
-def tally_cut_match(match, players, id_list):
-    
-    if match['player1']['winner']:
-        winner = 'player1'
-        loser = 'player2'
-    else:
-        winner = 'player2'
-        loser = 'player2'
-        
-    id_list[players.get_identity(player_id=match[winner]['id'], role=match[winner]['role'])]['wins'] += 1
-    id_list[players.get_identity(player_id=match[loser]['id'], role=match[loser]['role'])]['loses'] += 1
-
-
-def tally_results(rounds, players_dict):
-    
-    players = Players(players_dict)
-    id_list = create_id_list(players_dict)
-    
-    for round in rounds:
-        for match in round:
-            if match['eliminationGame']: 
-                tally_cut_match(match=match, players=players, id_list=id_list)
-            else:
-                tally_swiss_match(match=match, players=players, id_list=id_list)
-
-    return id_list
-    
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cobra
 import csv
 
+results_by_identity = cobra.Results()
+
 with open('config.txt') as config:
     for url in config:
         cobra_json = cobra.get_cobra_json(url)
-        results = cobra.tally_results(cobra_json['rounds'], cobra_json['players'])
+        results_by_identity.tally_results(cobra_json['rounds'], cobra_json['players'])
         
-        with open(cobra_json['name']+'.csv','w') as f:
-            w = csv.writer(f)
-            w.writerows(results.items())
+        print(results_by_identity.identities.identities)
+        
+        # with open(cobra_json['name']+'.csv','w') as f:
+        #     w = csv.writer(f)
+        #     w.writerows(results.items())
