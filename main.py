670ecf14e65708f6e620344a3f26ce28fba87852
not_yeti <ewanmilneu@gmail.com>
2024-05-06 18:41:03 +0100

    file name is now the name of the tournament not the id

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ cobra_json = cobra.get_cobra_json(url)
 
 results = cobra.tally_results(cobra_json['rounds'], cobra_json['players'])
         
-with open(url[-4:]+'.csv','w') as f:
+with open(cobra_json['name']+'.csv','w') as f:
     w = csv.writer(f)
     w.writerows(results.items())
