f740211ad441f74371159e50a0eab64a028dda3d
not_yeti <ewanmilneu@gmail.com>
2024-05-06 19:54:47 +0100

    Added a config file and support for multiple cobra urls at once

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import cobra
 import csv
 
-url ='https://tournaments.nullsignal.games/tournaments/3419'
-
-cobra_json = cobra.get_cobra_json(url)
-
-results = cobra.tally_results(cobra_json['rounds'], cobra_json['players'])
+with open('config.txt') as config:
+    for url in config:
+        cobra_json = cobra.get_cobra_json(url)
+        results = cobra.tally_results(cobra_json['rounds'], cobra_json['players'])
         
-with open(cobra_json['name']+'.csv','w') as f:
-    w = csv.writer(f)
-    w.writerows(results.items())
+        with open(cobra_json['name']+'.csv','w') as f:
+            w = csv.writer(f)
+            w.writerows(results.items())
