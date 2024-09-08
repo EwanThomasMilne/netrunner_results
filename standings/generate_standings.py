51b370ca4e039180a7d1bc930d32f5de9865e933
Ben Albon <ben@albon.me.uk>
2024-09-08 10:49:53 +0100

    handle unknown identity names

--- standings/generate_standings.py
+++ standings/generate_standings.py
@@ -38,11 +38,11 @@ def return_standing(json, player, tournament_sw: str, id_info):
             player_results = return_player_results_aesops(player['id'], json)
         
     runner_id = standardise_identity(player['runnerIdentity'])
-    runner_faction = id_info[runner_id].get('faction','unknown')
-    runner_name = id_info[runner_id].get('short_name','unknown')
+    runner_faction = id_info.get(runner_id,{}).get('faction','unknown')
+    runner_name = id_info.get(runner_id,{}).get('short_name','unknown')
     corp_id = standardise_identity(player['corpIdentity'])
-    corp_faction = id_info[corp_id].get('faction','unknown')
-    corp_name = id_info[corp_id].get('short_name','unknown')
+    corp_faction = id_info.get(corp_id,{}).get('faction','unknown')
+    corp_name = id_info.get(corp_id,{}).get('short_name','unknown')
 
     if is_player_in_top_cut(json['eliminationPlayers'], player['id']):
         top_cut_rank = find_player_in_top_cut(json['eliminationPlayers'], player['id']).get('rank')
