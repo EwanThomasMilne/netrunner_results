836307f3ef1a212a71c948efeb5312c249753723
Ben Albon <ben@albon.me.uk>
2024-09-18 12:17:51 +0100

    use sync-bre.csv to build players.yml

--- src/harvester.py
+++ src/harvester.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import csv
+import yaml
 from pathlib import Path
 
 def get_decklists_by_date(date: datetime.date):
@@ -14,8 +15,33 @@ def daterange(start_date: datetime.date, end_date: datetime.date):
     for n in range(days):
         yield start_date + datetime.timedelta(n)
 
+def load_bre_csv()->dict:
+    players = {}
+    csv_filepath = Path('OUTPUT/sync_bre.csv')
+    with csv_filepath.open(mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            try:
+                nrdb_id = int(line[6])
+                if nrdb_id not in players:
+                    players[nrdb_id] = {}
+                for alias in [line[0],line[1],line[2]]:
+                    if alias != "":
+                        if not players[nrdb_id].get('aliases'):
+                            players[nrdb_id]['aliases'] = []
+                        players[nrdb_id]['aliases'].append(alias)
+                for team in [line[3],line[4],line[5]]:
+                    if team != "":
+                        if not players[nrdb_id].get('teams'):
+                            players[nrdb_id]['teams'] = []
+                        players[nrdb_id]['teams'].append(team)
+            except ValueError:
+                pass
+    return players
+
 start_date = datetime.date(2024,1,1)
 end_date = datetime.date(2024,9,12)
+players = load_bre_csv()
 nrdb_ids = {}
 
 for date in daterange(start_date, end_date):
@@ -27,6 +53,9 @@ for date in daterange(start_date, end_date):
         username = decklist.get('user_name')
         userid = decklist.get('user_id')
         nrdb_ids.update({username:userid})
+        if not players.get(userid):
+            players[userid]={}
+        players[userid].update({'nrdb_name': username})
 
 csv_filepath = Path('OUTPUT/nrdb_ids.csv')
 csv_filepath.parent.mkdir(exist_ok=True, parents=True)
@@ -34,3 +63,7 @@ with csv_filepath.open(mode='w') as csv_file:
     csv_writer = csv.writer(csv_file, quotechar='"', quoting=csv.QUOTE_ALL, escapechar='\\')
     for name, id in nrdb_ids.items():
         csv_writer.writerow([name,id])
+
+yaml_filepath=Path('players.yml')
+with yaml_filepath.open(mode='w') as players_file:
+    yaml.dump(players, players_file)
