3240668501e5dd9dc350a07ffbe80803ba959499
Ben Albon <ben@albon.me.uk>
2024-09-25 12:56:46 +0100

    code tidy up

--- src/netrunner_results.py
+++ src/netrunner_results.py
@@ -18,6 +18,14 @@ def get_json(url: str, force: bool = False) -> dict:
     with json_filepath.open(mode='r') as json_file:
         return json.load(json_file)
     
+def write_standings_to_csv(standings: list, standings_filepath: Path):
+    standings_filepath.parent.mkdir(exist_ok=True, parents=True)
+    with standings_filepath.open(mode='w',newline='') as sf:
+        sw = csv.writer(sf, quotechar='"', quoting=csv.QUOTE_ALL, escapechar='\\')
+        sw.writerow(standings_header)
+        sw.writerows(standings)
+
+    
 def write_player_json_to_file(player: Player, filepath: Path):
     json_data = {'nrdb_id': player.nrdb_id, 'nrdb_name': player.nrdb_name, 'aliases': player.aliases, 'teams': player.teams, 'tournaments': player.tournaments}
     filepath.parent.mkdir(exist_ok=True, parents=True)
@@ -30,16 +38,16 @@ with open('tournaments.yml', 'r') as tournaments_file:
     standings_dir = 'OUTPUT/standings/'
     standings_header = ['date','region','online','tournament_id','tournament','top_cut_rank','swiss_rank','name','team 1','team 2','team 3','corp_name','corp_wins','corp_losses','corp_draws','runner_name','runner_wins','runner_losses','runner_draws','matchPoints','SoS','xSoS','corp_ID','corp_faction','runner_ID','runner_faction','nrdb_id']
     allstandings_filepath = Path('OUTPUT/allstandings.csv')
+    allstandings_filepath.parent.mkdir(exist_ok=True, parents=True)
 
     results_dir = 'OUTPUT/results/'
     results_header = [ 'date','meta','region','online','tournament_id','tournament','phase','round','table','corp_player','corp_id','result','runner_player','runner_id']
     allresults_filepath = Path('OUTPUT/allresults.csv')
+    allresults_filepath.parent.mkdir(exist_ok=True, parents=True)
 
     player_dir = 'OUTPUT/players/'
     players = {}
 
-    allstandings_filepath.parent.mkdir(exist_ok=True, parents=True)
-    allresults_filepath.parent.mkdir(exist_ok=True, parents=True)
     with allstandings_filepath.open(mode='w',newline='') as allstandings_file, allresults_filepath.open(mode='w',newline='') as allresults_file:
         allstandings_writer = csv.writer(allstandings_file, quotechar='"', quoting=csv.QUOTE_ALL, escapechar='\\')
         allstandings_writer.writerow(standings_header)
@@ -53,22 +61,25 @@ with open('tournaments.yml', 'r') as tournaments_file:
                 continue
             for tournament in tournaments:
                 tournament_json = get_json(tournament['url'])
+                tournament_name = tournament.get('name',tournament_json['name'])
+                print('TOURNAMENT: ' + tournament_name + ' [' + tournament['url'] + ']' )
+
                 if 'aesop' in tournament['url']:
                     software = 'aesop'
-                    t = AesopsTournament(name=tournament.get('name',None),json=tournament_json,date=tournament.get('date',None),region=tournament.get('region',None),online=tournament.get('online',False),player_mappings=tournament.get('players',None))
+                    t = AesopsTournament(name=tournament_name,json=tournament_json,date=tournament.get('date',None),region=tournament.get('region',None),online=tournament.get('online',False),player_mappings=tournament.get('players',None))
                 else:
                     software = 'cobra'
-                    t = CobraTournament(name=tournament.get('name',None),json=tournament_json,date=tournament.get('date',None),region=tournament.get('region',None),online=tournament.get('online',False),player_mappings=tournament.get('players',None))
-                print('TOURNAMENT: ' + t.name + ' [' + tournament['url'] + ']' )
-
+                    t = CobraTournament(name=tournament_name,json=tournament_json,date=tournament.get('date',None),region=tournament.get('region',None),online=tournament.get('online',False),player_mappings=tournament.get('players',None))
+                
+                # useful variables
                 if t.online is True:
                     online = "netspace"
                 else:
                     online = "meatspace"
-
                 tournament_number = tournament['url'].rsplit('/', 1)[-1]
                 tournament_id = software + '-' + tournament_number
 
+                # standings
                 standings = t.standings
                 for row in standings:
                     row.insert(0,t.date)
@@ -79,12 +90,9 @@ with open('tournaments.yml', 'r') as tournaments_file:
                     allstandings_writer.writerow(row)
 
                 standings_filepath = Path(standings_dir + str(t.date) + '.' + tournament_id + '.standings.csv')
-                standings_filepath.parent.mkdir(exist_ok=True, parents=True)
-                with standings_filepath.open(mode='w',newline='') as sf:
-                    sw = csv.writer(sf, quotechar='"', quoting=csv.QUOTE_ALL, escapechar='\\')
-                    sw.writerow(standings_header)
-                    sw.writerows(standings)
+                write_standings_to_csv(standings=t.standings, standings_filepath=standings_filepath)
 
+                # results
                 results = t.results
                 results_filename = results_dir + str(t.date) + '.' + tournament_id + '.results.csv'
                 with open(results_filename,'w',newline='') as rf:
@@ -95,6 +103,7 @@ with open('tournaments.yml', 'r') as tournaments_file:
                         allresults_writer.writerow(row)
                         rw.writerow(row)
 
+                # players
                 for id,t_player in t.players.items():
                     if t_player.nrdb_id:
                         if not players.get(t_player.nrdb_id):
